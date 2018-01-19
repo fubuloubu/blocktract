54ed0304763e6767eaf42c2698160213d4a74bdd
Bryant <fubuloubu@gmail.com>
2018-01-19 02:12:09 -0500

    Added some comments about operation

--- blocktract/compiler.py
+++ blocktract/compiler.py
@@ -15,23 +15,42 @@ def _compile(raw_code: str,
              debug_vy_abi=False,
              debug_lll_ir=False,
              optimize=0) -> dict:
+
+    # 1. Python parses to it's own AST
     py_ast = ast.parse(raw_code)
     if debug_py_ast:
         debug('Python AST', ast2objtree(py_ast, ast.AST), level=5)
+
+    # 2. We Transform Python's AST to custom Vyper AST classes
+    #    This filters out additional Python features and cleans our AST
     vy_ast = transform(py_ast)
     if debug_vy_ast:
         debug('Vyper AST', ast2objtree(vy_ast, vyAST), level=5)
+
+    # 3. At this point we can obtain the ABI directly from AST
     abi = get_abi(vy_ast)
     if debug_vy_abi:
         debug('Application Binary Interface', abi, level=5)
+
+    # Maybe optimize Vyper AST?
+
+    # 4. Compiler the AST into our IR, which is LLL
+    #    This produces a few different outputs
     lll_code = ast_to_lll(vy_ast)
     if debug_lll_ir:
         debug('LLL IR', lll_code, level=5)
+
+    # 5. (optional) Run optimizations on LLL IR
+    #    This should not modify functionality, only reducing gas costs
     if optimize > 0:
         lll_code = optimize_lll(lll_code)
     if debug_lll_ir:
         debug('Optimized LLL IR', lll_code, level=5)
+
+    # 6. Last, translate this from LLL IR to EVM Bytecode
     bytecode, runtime = compile_lll(lll_code)
+
+    # Returns JSON-compatible format for our deliverables
     return {
             'abi': abi, 
             'bin': "0x"+bytecode.hex(),
